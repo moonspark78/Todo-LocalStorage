3274db8ed878919929c71fd1c8d8dbed5766baff
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-03-15 07:36:37 +0100

    📈 working on the js  📈

---
+++ script.js
@@ -0,0 +1,6 @@
+const inputTask = document.getElementById("inputTask");
+const addBtn = document.getElementById("addBtn")
+const taskList = document.getElementById("taskList")
+
+// Événements pour le bouton et l'input
+addBtn.addEventListener('click', addTask);
